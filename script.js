049e0ece1fa7c12568a3556f6f2d38f0f51f5fe3
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-06-08 02:09:14 +0700

    link api

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const icon = document.querySelector('icon')
-const search = document.querySelector('search')
-icon.addEventListener('onclick', function() {
-    search.classList.toggle('active')
+document.getElementById('searchButton').addEventListener('click', () => {
+    let id = document.getElementById('inputText').value
+    console.log(id)
+    fetch(`https://api.jikan.moe/v3/search/anime?q=` + id)
+        .then(response => {
+            console.log(response)
+            return response.json()
+        }).then(results => {
+            console.log(results.results)
+        })
+
 })
+
+function showResults() {
+    fetch(`https://api.jikan.moe/v3/search/anime?q=` + id)
+        .then(response => {
+            console.log(response)
+            return response.json()
+        }).then(results => {
+            return results.title
+        })
+}
