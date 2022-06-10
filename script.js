3213885a507ab115ea3cb4c8461ba08841d883ab
AlpacaUniix <supatsara_wan@cmu.ac.th>
2022-06-11 01:32:17 +0700

    display element and edit nav bar

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
+let keyword = undefined
+let anime = undefined
 document.getElementById('searchButton').addEventListener('click', () => {
     let id = document.getElementById('inputText').value
     console.log(id)
     fetch(`https://api.jikan.moe/v3/search/anime?q=` + id)
         .then(response => {
-            console.log(response)
             return response.json()
         }).then(results => {
             console.log(results.results)
+            keyword = results.results
+            addAnimeList(keyword)
         })
-
 })
 
-function showResults() {
-    fetch(`https://api.jikan.moe/v3/search/anime?q=` + id)
-        .then(response => {
-            console.log(response)
-            return response.json()
-        }).then(results => {
-            return results.title
-        })
+function addAnimeList(animeList) {
+    let counter = 1
+    const tableBody = document.getElementById('tableBody')
+    tableBody.innerHTML = ''
+    for (keyword of animeList) {
+        showAnimeOnTable(counter++, keyword)
+    }
+}
+
+function showAnimeOnTable(index, keyword) {
+    const tableBody = document.getElementById('tableBody')
+    let row = document.createElement('tr')
+    let cell = document.createElement('th')
+        // cell.setAttribute('')
+    cell.innerHTML = index
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = keyword.title
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    let img = document.createElement('img')
+    img.setAttribute('src', keyword.image_url)
+    img.height = 300
+    cell.appendChild(img)
+    row.appendChild(cell)
+    cell = document.createElement('td')
+    cell.innerHTML = keyword.synopsis
+    row.appendChild(cell)
+    tableBody.appendChild(row)
 }
